d96fd86a38fb6501f1a4de76c585f4fea0e6f3f1
Jordy Anderson Celestino Mendoza <Jordy.Ander@hotmail.com>
2020-08-27 02:31:28 -0400

    Programa de hola mundo pero depurado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HolaMundo_master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nCual es tu nombre? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHola, {name}, on {date:d} at {date:t}!");
+            Console.Write("Presione una tecla para salir");
+            Console.ReadKey(true);
+        }
+    }
+}
